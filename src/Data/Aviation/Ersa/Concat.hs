9e0ba4fd9d026de6eb5aea79de5bd651cf64a05e
Tony Morris <tmorris@tmorris.net>
2016-09-18 09:28:23 +1000

    default ersas

--- src/Data/Aviation/Ersa/Concat.hs
+++ src/Data/Aviation/Ersa/Concat.hs
@@ -227,6 +227,19 @@ main =
                                                       ErsaCurrent
                                                     _ ->
                                                       ErsaPending)) >>= exitWith
+        [] ->
+          traverseExitCodes defaultersaconcat ersas >>= exitWith
         _ ->
-          do  hPutStrLn stderr "Enter at least three arguments for ERSA <page> <date (dd-Mmm-yyyy where Mmm is the first three letters of the Julian month name)> <version> [a fourth argument implies the ERSA is pending]"
+          do  hPutStrLn stderr "Enter zero arguments for default ERSA list or three arguments for single ERSA <page> <date (dd-Mmm-yyyy where Mmm is the first three letters of the Julian month name)> <version> [a fourth argument implies the ERSA is pending]"
               exitWith (ExitFailure 65535)
+
+----
+
+ersas ::
+  [ErsaDocument]
+ersas =
+  [
+    ErsaDocument "40" "18-Aug-2016" "1" ErsaCurrent
+  , ErsaDocument "40" "10-Nov-2016" "0" ErsaPending
+  , ErsaDocument "40" "10-Nov-2016" "2" ErsaPending
+  ]
